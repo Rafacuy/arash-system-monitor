ba00ea3a3d5f85c0075086780147f77b4bd94d42
Rafacuy <rafardhanganteng91@gmail.com>
2025-04-15 20:28:22 +0700

    Added LICENSE, README.md

--- Utils/clock.py
+++ Utils/clock.py
@@ -4,7 +4,6 @@ from datetime import datetime
 import pytz
 
 def update_clock(label, root):
-    jakarta_tz = pytz.timezone('Asia/Jakarta')
-    current_time = datetime.now(jakarta_tz).strftime("%H:%M:%S")
+    current_time = datetime.now().strftime("%H:%M:%S")
     label.config(text=current_time)
     root.after(1000, lambda: update_clock(label, root))
